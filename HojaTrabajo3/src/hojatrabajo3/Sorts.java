75d9b15fc03608d34f3ebda288a9f4a6847486b9
andresum97 <andresum1309@gmail.com>
2018-02-08 21:52:18 -0600

    Modificaciones de sorts a Comparable

--- HojaTrabajo3/src/hojatrabajo3/Sorts.java
+++ HojaTrabajo3/src/hojatrabajo3/Sorts.java
@@ -15,40 +15,42 @@ public class Sorts {
      * Método de Radixsort, ordena los números verificando por unidad, decena y demás dependiendo del tamaño del valor
      * @param x  Array donde se almacenaran los datos
      */
-    public static void Radixsort(int[] x)
-    {
-	int inicio = x[0];
+   public static void Radixsort(Comparable[] x)
+{
+	Comparable inicio = x[0];
 	int exp = 1;
 	int largo = x.length;
-	int [] digitos = new int[10];
+	Comparable [] digitos = new Comparable[10];
 	for(int i=1;i<largo;i++){
-		if(x[i]>inicio){
+		if(x[i].compareTo(inicio)>0)//if(x[i]>inicio){
 			inicio = x[i];
 		}
-	}
-	while(inicio/exp >0){
+	while((int)inicio/exp >0){
 		int[] temporal = new int[10];
 		for(int i=0;i<largo;i++)
-			temporal[(x[i]/exp)%10]++;
+			temporal[((int)x[i]/exp)%10]++;
 		for(int i=1;i<10;i++)
 			temporal[i] += temporal[i-1];
 		for(int i=largo-1;i>=0;i--)
-			digitos[--temporal[(x[i]/exp)%10]] = x[i];
+			digitos[--temporal[((int)x[i]/exp)%10]] = x[i];
 		for(int i=0; i<largo; i++)
 			x[i] = digitos[i];
 		exp *=10;
 	}
 }
-
-      public void Bubblesort(int[] x){
+/**
+ * Método de Bubblesort, ordena los valores intercambiando de posición los valores dependiendo quien es mayor
+ * @param x Array donde se almacenan los datos
+ */
+public void Bubblesort(Comparable[] x){
 	boolean estado = true;
 	int j = 0;
-	int temporal;
+	Comparable temporal = null;
 	while(estado){
 		estado = false;
 		j++;
 		for(int i = 0; i < x.length - j; i++){
-			if(x[i] > x[i+1]){
+			if(x[i].compareTo(x[i+1])>0)//if(x[i] > x[i+1]){
 				temporal = x[i];
 				x[i] = x[i+1];
 				x[i+1] = temporal;
@@ -58,5 +60,4 @@ public class Sorts {
 	}
 
 }
-    
-}
+   
